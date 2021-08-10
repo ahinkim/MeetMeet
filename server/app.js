7166d52b78f8b730593994e43bf70be7053dfdbf
ahin <ahin98@naver.com>
2021-08-10 14:06:30 +0900

    Modify login server4

--- server/app.js
+++ server/app.js
@@ -100,6 +100,7 @@ function connectDB() {
     });
 }
 
+var user = require('./routes/user');
 //user 스키마 및 모델 객체 생성
 function createUserSchema(){
 
@@ -109,6 +110,9 @@ function createUserSchema(){
     // UserModel 모델 정의
     UserModel = mongoose.model("users", UserSchema);
     console.log('UserModel 정의함.');
+    
+    //init 호출
+    user.init(database, UserSchema, UserModel);
 }	
 
 
@@ -119,215 +123,16 @@ function createUserSchema(){
 var router = express.Router();
 
 // 로그인 라우팅 함수 - 데이터베이스의 정보와 비교
-router.route('/process/login').post(function(req, res) {
-	console.log('/process/login 호출됨.');
-
-	// 요청 파라미터 확인
-    var paramId = req.body.id || req.query.id;
-    var paramPassword = req.body.password || req.query.password;
-	
-    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
-	
-    // 데이터베이스 객체가 초기화된 경우, authUser 함수 호출하여 사용자 인증
-	if (database) {
-		authUser(database, paramId, paramPassword, function(err, docs) {
-			if (err) {throw err;}
-			
-            // 조회된 레코드가 있으면 성공 응답 전송
-			if (docs) {
-				console.dir(docs);
-
-                // 조회 결과에서 사용자 이름 확인
-				//var result = new Boolean(true);
-				res.status(200).json({ success: true });
-				res.end();
-			
-			} else {  // 조회된 레코드가 없는 경우 실패 응답 전송
-				res.status(200).json({ success: false });
-				res.end();
-			}
-		});
-	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
-		res.status(200);
-        res.write('데이터베이스 연결 실패');
-        res.end();
-	}
-	
-});
-
-
+router.route('/process/login').post(user.login);
 
 // 사용자 추가 라우팅 함수 - 클라이언트에서 보내오는 데이터를 이용해 데이터베이스에 추가
-router.route('/process/adduser').post(function(req, res) {
-	console.log('/process/adduser 호출됨.');
+router.route('/process/adduser').post(user.adduser);
 
-    var paramId = req.body.id || req.query.id;
-    var paramPassword = req.body.password || req.query.password;
-	var paramNickname = req.body.nickname || req.query.nickname;
-    
-    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword );
-    
-    // 데이터베이스 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
-	if (database) {
-		addUser(database, paramId, paramPassword, paramNickname, function(err, addedUser) {
-			if (err) {
-                res.status(400);
-                throw err;
-            }
-			
-            // 결과 객체 있으면 성공 응답 전송
-			if (addedUser) {
-                
-				console.dir(addedUser);
-                res.status(200);
-                res.write("사용자 등록 성공");
-				res.end();
-                
-			} else {  // 결과 객체가 없으면 실패 응답 전송
-                
-                res.status(400);
-                res.write("사용자 등록 실패");
-				res.end();
-			}
-		});
-	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
-		//res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-		res.status(200);
-        res.write('데이터베이스 연결 실패');
-		res.end();
-	}
-	
-});
-
-router.route('/process/validate').post(function(req, res) {
-	console.log('/process/validate 호출됨.');
-
-    var paramId = req.body.id || req.query.id;
-
-    console.log('요청 파라미터 : ' + paramId );
-    
-    // 데이터베이스 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
-	if (database) {
-        validateUser(database, paramId, function(err, result) {
-			if (err) { 
-                res.status(400);
-                throw err;
-            }
-			
-            // 결과 객체 있으면 성공 응답 전송
-			if (result) {
-                res.status(200).json({ success: false });
-				res.end();
-                
-			} else {  // 결과 객체가 없으면 실패 응답 전송
-                
-                res.status(200).json({ success: true });
-				res.end();
-			}
-		});
-	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
-		//res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-		res.status(200);
-        res.write('데이터베이스 연결 실패');
-		res.end();
-	}
-	
-});
+router.route('/process/validate').post(user.validate);
 
 
 // 라우터 객체 등록
 app.use('/', router);
-
-
-
-// 사용자를 인증하는 함수
-var authUser = function(database, id, password, callback) {
-	console.log('authUser 호출됨.');
-	
-    //1.아이디를 사용해 검색
-    UserModel.findById(id,function(err,results){
-        if(err){
-            callback(err,null);
-            return;
-        }
-        
-        console.log('아이디 [%s]로 사용자 검색결과', id);
-        console.dir(results);
-        
-        if (results.length > 0) { 
-	    	console.log('아이디와 일치하는 사용자 찾음.');
-	 
-    
-            //2. 비밀번호 확인
-            if(results[0]._doc.password == password){
-                console.log('비밀번호 일치함.');
-                callback(null, results);
-            }else{
-                console.log('비밀번호 일치하지 않음.');
-                callback(null, null);
-            }
-        }else{
-            console.log("아이디와 일치하는 사용자를 찾지 못함.");
-            callback(null,null);
-        }
-        
-    });
-}
-
-
-//사용자를 추가하는 함수
-var addUser = function(database, id, password,nickname, callback) {
-	console.log('addUser 호출됨 : ' + id + ', ' + password + ', '+ nickname );
-	
-    UserModel.find({"id":id}, function(err, results) {
-
-    if (err) {  // 에러 발생 시 콜백 함수를 호출하면서 에러 객체 전달
-        callback(err, null);
-        return;
-    }
-
-    // UserModel 인스턴스 생성
-    var user = new UserModel({"id":id, "password":password, "nickname":nickname});
-
-    // save()로 저장 : 저장 성공 시 addedUser 객체가 파라미터로 전달됨
-    user.save(function(err, addedUser) {
-    if (err) {
-        callback(err, null);
-        return;
-    }
-
-    console.log("사용자 데이터 추가함.");
-    callback(null, addedUser);
-
-        });
-    })
-};
-
-
-
-//사용자 중복확인 하는 함수
-var validateUser = function(database, id, callback) {
-	console.log('validateUser 호출됨 : ' + id );
-	
-    UserModel.find({"id":id}, function(err, results) {
-
-        if (err) {  // 에러 발생 시 콜백 함수를 호출하면서 에러 객체 전달
-            callback(err, null);
-            return;
-        }
-
-        if (results.length > 0) { 
-            // 조회한 레코드가 있는 경우 콜백 함수를 호출하면서 조회 결과 전달
-                console.log('아이디 중복');
-                callback(null, true);
-                return;
-
-            } else{
-                console.log('아이디 중복되지 않음');
-                callback(null, null);
-            }
-    })
-};
     
 
 // 404 에러 페이지 처리
--- server/routes/user.js
+++ server/routes/user.js
@@ -20,12 +20,9 @@ var init = function(db, schema, model) {
 	UserModel = model;
 }
 
-
 var login = function(req, res) {
-	console.log('user 모듈 안에 있는 login 호출됨.');
+	console.log('user모듈 안에 있는 login 호출됨.');
 
-    // 데이터베이스 객체가 필요한 경우 req.app.get('database')로도 참조 가능
-    
 	// 요청 파라미터 확인
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
@@ -35,219 +32,211 @@ var login = function(req, res) {
     // 데이터베이스 객체가 초기화된 경우, authUser 함수 호출하여 사용자 인증
 	if (database) {
 		authUser(database, paramId, paramPassword, function(err, docs) {
-			// 에러 발생 시, 클라이언트로 에러 전송
-			if (err) {
-                console.error('로그인 중 에러 발생 : ' + err.stack);
-                
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h2>로그인 중 에러 발생</h2>');
-                res.write('<p>' + err.stack + '</p>');
-				res.end();
-                
-                return;
-            }
+			if (err) {throw err;}
 			
             // 조회된 레코드가 있으면 성공 응답 전송
 			if (docs) {
 				console.dir(docs);
 
                 // 조회 결과에서 사용자 이름 확인
-				var username = docs[0].name;
-				
-				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h1>로그인 성공</h1>');
-				res.write('<div><p>사용자 아이디 : ' + paramId + '</p></div>');
-				res.write('<div><p>사용자 이름 : ' + username + '</p></div>');
-				res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+				//var result = new Boolean(true);
+				res.status(200).json({ success: true });
 				res.end();
 			
 			} else {  // 조회된 레코드가 없는 경우 실패 응답 전송
-				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h1>로그인  실패</h1>');
-				res.write('<div><p>아이디와 패스워드를 다시 확인하십시오.</p></div>');
-				res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+				res.status(200).json({ success: false });
 				res.end();
 			}
 		});
 	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
-		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-		res.write('<h2>데이터베이스 연결 실패</h2>');
-		res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>');
-		res.end();
+		res.status(200);
+        res.write('데이터베이스 연결 실패');
+        res.end();
 	}
 	
-};
+}
 
 var adduser = function(req, res) {
-	console.log('user 모듈 안에 있는 adduser 호출됨.');
-
-	// 필요한 경우 req.app.get('database')로도 참조 가능
+	console.log('user모듈 안에 있는 adduser 호출됨.');
 
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
-    var paramName = req.body.name || req.query.name;
-	
-    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword + ', ' + paramName);
+	var paramNickname = req.body.nickname || req.query.nickname;
+    
+    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword );
     
     // 데이터베이스 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
 	if (database) {
-		addUser(database, paramId, paramPassword, paramName, function(err, addedUser) {
-            // 동일한 id로 추가하려는 경우 에러 발생 - 클라이언트로 에러 전송
+		addUser(database, paramId, paramPassword, paramNickname, function(err, addedUser) {
 			if (err) {
-                console.error('사용자 추가 중 에러 발생 : ' + err.stack);
-                
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h2>사용자 추가 중 에러 발생</h2>');
-                res.write('<p>' + err.stack + '</p>');
-				res.end();
-                
-                return;
+                res.status(400);
+                throw err;
             }
 			
             // 결과 객체 있으면 성공 응답 전송
 			if (addedUser) {
+                
 				console.dir(addedUser);
- 
-				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h2>사용자 추가 성공</h2>');
+                res.status(200);
+                res.write("사용자 등록 성공");
 				res.end();
+                
 			} else {  // 결과 객체가 없으면 실패 응답 전송
-				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h2>사용자 추가  실패</h2>');
+                
+                res.status(400);
+                res.write("사용자 등록 실패");
 				res.end();
 			}
 		});
 	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
-		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-		res.write('<h2>데이터베이스 연결 실패</h2>');
+		//res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.status(200);
+        res.write('데이터베이스 연결 실패');
 		res.end();
 	}
 	
-};
+}
 
-var listuser = function(req, res) {
-	console.log('user 모듈 안에 있는 listuser 호출됨.');
+var validate = function(req, res) {
+	console.log('user 모듈 안에 있는 validate 호출됨.');
 
-	// 필요한 경우 req.app.get('database')로도 참조 가능
-	 
-	
-	// 데이터베이스 객체가 초기화된 경우, 모델 객체의 findAll 메소드 호출
+    var paramId = req.body.id || req.query.id;
+
+    console.log('요청 파라미터 : ' + paramId );
+    
+    // 데이터베이스 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
 	if (database) {
-		// 1. 모든 사용자 검색
-		UserModel.findAll(function(err, results) {
-			// 에러 발생 시, 클라이언트로 에러 전송
-			if (err) {
-                console.error('사용자 리스트 조회 중 에러 발생 : ' + err.stack);
-                
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h2>사용자 리스트 조회 중 에러 발생</h2>');
-                res.write('<p>' + err.stack + '</p>');
-				res.end();
-                
-                return;
+        validateUser(database, paramId, function(err, result) {
+			if (err) { 
+                res.status(400);
+                throw err;
             }
-			  
-			if (results) {  // 결과 객체 있으면 리스트 전송
-				console.dir(results);
- 
-				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h2>사용자 리스트</h2>');
-				res.write('<div><ul>');
-				
-				for (var i = 0; i < results.length; i++) {
-					var curId = results[i]._doc.id;
-					var curName = results[i]._doc.name;
-					res.write('    <li>#' + i + ' : ' + curId + ', ' + curName + '</li>');
-				}	
 			
-				res.write('</ul></div>');
+            // 결과 객체 있으면 성공 응답 전송
+			if (result) {
+                res.status(200).json({ success: false });
 				res.end();
+                
 			} else {  // 결과 객체가 없으면 실패 응답 전송
-				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h2>사용자 리스트 조회  실패</h2>');
+                
+                res.status(200).json({ success: true });
 				res.end();
 			}
 		});
 	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
-		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-		res.write('<h2>데이터베이스 연결 실패</h2>');
-		res.end(); 
+		//res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.status(200);
+        res.write('데이터베이스 연결 실패');
+		res.end();
 	}
 	
-};
-
-
-//필요한 경우 req.app.get('database')로 참조 가능 : 이 파일에서는 사용하지 않음
-var checkDatabase = function(req) {
-	if (!database) {
-		console.log('user 모듈에서 데이터베이스 객체를 참조합니다.');
-		
-		database = req.app.get('database');
-		UserSchema = req.app.get('UserSchema');
-		UserModel = req.app.get('UserModel');
-	} else {
-		console.log('user 모듈에서 데이터베이스 객체를 이미 참조했습니다.');
-	}
 }
 
-
-// 사용자를 인증하는 함수 : 아이디로 먼저 찾고 비밀번호를 그 다음에 비교하도록 함
+// 사용자를 인증하는 함수
 var authUser = function(database, id, password, callback) {
-	console.log('authUser 호출됨 : ' + id + ', ' + password);
+	console.log('authUser 호출됨.');
 	
-    // 1. 아이디를 이용해 검색
-	UserModel.findById(id, function(err, results) {
-		if (err) {
-			callback(err, null);
-			return;
-		}
-		
-		console.log('아이디 [%s]로 사용자 검색결과', id);
-		console.dir(results);
-		
-		if (results.length > 0) {
-			console.log('아이디와 일치하는 사용자 찾음.');
-			
-			// 2. 패스워드 확인 : 모델 인스턴스를 객체를 만들고 authenticate() 메소드 호출
-			var user = new UserModel({id:id});
-			var authenticated = user.authenticate(password, results[0]._doc.salt, results[0]._doc.hashed_password);
-			if (authenticated) {
-				console.log('비밀번호 일치함');
-				callback(null, results);
-			} else {
-				console.log('비밀번호 일치하지 않음');
-				callback(null, null);
-			}
-			
-		} else {
-	    	console.log("아이디와 일치하는 사용자를 찾지 못함.");
-	    	callback(null, null);
-	    }
-		
-	});
-	
+    //1.아이디를 사용해 검색
+    UserModel.findById(id,function(err,results){
+        if(err){
+            callback(err,null);
+            return;
+        }
+        
+        console.log('아이디 [%s]로 사용자 검색결과', id);
+        console.dir(results);
+        
+        if (results.length > 0) { 
+	    	console.log('아이디와 일치하는 사용자 찾음.');
+	 
+    
+            //2. 비밀번호 확인
+            if(results[0]._doc.password == password){
+                console.log('비밀번호 일치함.');
+                callback(null, results);
+            }else{
+                console.log('비밀번호 일치하지 않음.');
+                callback(null, null);
+            }
+        }else{
+            console.log("아이디와 일치하는 사용자를 찾지 못함.");
+            callback(null,null);
+        }
+        
+    });
 }
 
 
 //사용자를 추가하는 함수
-var addUser = function(database, id, password, name, callback) {
-	console.log('addUser 호출됨 : ' + id + ', ' + password + ', ' + name);
+var addUser = function(database, id, password,nickname, callback) {
+	console.log('addUser 호출됨 : ' + id + ', ' + password + ', '+ nickname );
 	
-	// UserModel 인스턴스 생성
-	var user = new UserModel({"id":id, "password":password, "name":name});
+    UserModel.find({"id":id}, function(err, results) {
 
-	// save()로 저장 : 저장 성공 시 addedUser 객체가 파라미터로 전달됨
-	user.save(function(err, addedUser) {
-		if (err) {
-			callback(err, null);
-			return;
-		}
-		
-	    console.log("사용자 데이터 추가함.");
-	    callback(null, addedUser);
-	     
-	});
-}
+    if (err) {  // 에러 발생 시 콜백 함수를 호출하면서 에러 객체 전달
+        callback(err, null);
+        return;
+    }
+
+    // UserModel 인스턴스 생성
+    var user = new UserModel({"id":id, "password":password, "nickname":nickname});
+
+    // save()로 저장 : 저장 성공 시 addedUser 객체가 파라미터로 전달됨
+    user.save(function(err, addedUser) {
+    if (err) {
+        callback(err, null);
+        return;
+    }
+
+    console.log("사용자 데이터 추가함.");
+    callback(null, addedUser);
+
+        });
+    })
+};
+
+
+
+//사용자 중복확인 하는 함수
+var validateUser = function(database, id, callback) {
+	console.log('validateUser 호출됨 : ' + id );
+	
+    UserModel.find({"id":id}, function(err, results) {
+
+        if (err) {  // 에러 발생 시 콜백 함수를 호출하면서 에러 객체 전달
+            callback(err, null);
+            return;
+        }
+
+        if (results.length > 0) { 
+            // 조회한 레코드가 있는 경우 콜백 함수를 호출하면서 조회 결과 전달
+                console.log('아이디 중복');
+                callback(null, true);
+                return;
+
+            } else{
+                console.log('아이디 중복되지 않음');
+                callback(null, null);
+            }
+    })
+};
+
+
+////필요한 경우 req.app.get('database')로 참조 가능 : 이 파일에서는 사용하지 않음
+//var checkDatabase = function(req) {
+//	if (!database) {
+//		console.log('user 모듈에서 데이터베이스 객체를 참조합니다.');
+//		
+//		database = req.app.get('database');
+//		UserSchema = req.app.get('UserSchema');
+//		UserModel = req.app.get('UserModel');
+//	} else {
+//		console.log('user 모듈에서 데이터베이스 객체를 이미 참조했습니다.');
+//	}
+//}
+
+
+
 
 
 // module.exports 객체에 속성으로 추가
@@ -255,5 +244,5 @@ var addUser = function(database, id, password, name, callback) {
 module.exports.init = init;
 module.exports.login = login;
 module.exports.adduser = adduser;
-module.exports.listuser = listuser;
+module.exports.validate = validate;
 
