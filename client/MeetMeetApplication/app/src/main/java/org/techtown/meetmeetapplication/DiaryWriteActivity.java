acc0479352484b062af842918a1252d4fe144fab
HY <cyn1025@naver.com>
2021-08-20 21:42:08 +0900

    client UI 제작

---
+++ client/MeetMeetApplication/app/src/main/java/org/techtown/meetmeetapplication/DiaryWriteActivity.java
@@ -0,0 +1,20 @@
+package org.techtown.meetmeetapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.techtown.meetmeetapp.R;
+
+public class DiaryWriteActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_write );
+
+        
+
+    }
+}
